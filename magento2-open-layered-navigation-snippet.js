9c0bf4e01391b062725d113b745307eeeb70d4ec
aprwebdesign <Info@aprwebdesign.com>
2017-05-19 10:57:41 +0200

    Update magento2-open-layered-navigation-snippet.js

--- magento2-open-layered-navigation-snippet.js
+++ magento2-open-layered-navigation-snippet.js
@@ -1,3 +1,6 @@
+// When using the default layered navigation class names you can use this snippet
+// To change the state to open instead of the default state ( hidden)
+
 (function() {
  var elems = document.querySelectorAll('.catalog-category-view, .filter-options-content');
 for (var i=0;i<elems.length;i+=1){
