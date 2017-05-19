8e751b726a1712c9d2febd610410ce69ac1d890b
aprwebdesign <Info@aprwebdesign.com>
2017-05-19 10:56:28 +0200

    Update magento2-open-layered-navigation-snippet.js

--- magento2-open-layered-navigation-snippet.js
+++ magento2-open-layered-navigation-snippet.js
@@ -1,1 +1,6 @@
-jQuery('.catalog-category-view .filter-options-content').show(); 
+(function() {
+ var elems = document.querySelectorAll('.catalog-category-view, .filter-options-content');
+for (var i=0;i<elems.length;i+=1){
+  elems[i].style.display = 'block';
+}
+})();
